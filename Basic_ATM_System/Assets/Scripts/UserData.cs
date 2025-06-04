5fb211cf7d84dccc000d3a53a91ce47f6ae63377
ChunBae20 <drchunbae@gmail.com>
2025-06-04 13:25:34 +0900

    [feat] 기초 UI 완성 1단계
    [feat] 기초 UI 완성 1단계

---
+++ Basic_ATM_System/Assets/Scripts/UserData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData : MonoBehaviour
+{
+    private string userName;
+
+    //후에 시간남으면 내가만들어둔 시간시스템 건져와서 cash 다 decimal로 선언하고 이자같은거 실시간적용이나 코인한번 구현해보자
+    private int userCash;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
