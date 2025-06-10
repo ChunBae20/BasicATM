ab8379674349438b2468ad118ce3b68175e59746
ChunBae20 <drchunbae@gmail.com>
2025-06-10 19:50:41 +0900

    [Feat] 스크롤뷰 구성
    스크롤 할때 height수동으로 늘리지 않는이상 자동으로 안늘어나는건
    viewport-content에 add component의
    Content Size Fitter를 추가 안해줬기 때문임

--- Assets/Scripts/Inventory/UIInventoryIV.cs
+++ Assets/Scripts/Inventory/UIInventoryIV.cs
@@ -10,12 +10,43 @@ public class UIInventoryIV : MonoBehaviour
     [SerializeField]
     Button openMainMenuButton;
 
+    public GameObject slotPrefabs; //생성될 슬롯 프리팹들 기본 120개
+
+    //public UISlotIV uiSlots;
+
+    public Transform content; //스크롤뷰아래의 content를 부모로두고 자식을 프리팹으로 생성하면 되겟지..?
+
+    public List<UISlotIV> uiSlots = new List<UISlotIV>();
+
+    int basicUIslotCount = 120;
+    
+
     void Start()
     {
         openMainMenuButton.onClick.AddListener(OpenMainMenu);
+
+        SpawnSlots();
+
     }
     public void OpenMainMenu()
     {
         UIManagerIV.Instance.OpenMainMenu();
     }
+
+    void SpawnSlots()
+    {
+
+        for (int i =0;i< basicUIslotCount; i++)
+        {
+            uiSlots.Add(new UISlotIV());
+
+
+        }
+
+
+
+    }
+
+
+
 }
